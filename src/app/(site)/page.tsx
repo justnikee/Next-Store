c6b4e8a634b91b800bd800d154e72e6fd3ebd60b
justnikee <nikhil98161@gmail.com>
2023-05-09 17:54:03 +0530

    prisma-schema

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,11 +1,20 @@
+import prisma from "../../../lib/prisma"
+
+
+
 
 export default async function Home() {
-    const res = await fetch('api/createProducts')
-    const data = res.json()
-    console.log(data)
+    const products = await prisma.product.findMany()
+    console.log(products)
     return (
         <>
-           Home
+           {products.map((product) => (
+            <div className="flex flex-col items-center justify-center bg-white w-60 h-full p-6text-center" key={product.id}>
+                <span className="text-sm text-gray-600 text-center">Product Name: {product.name}</span>
+                <span className="text-sm text-gray-600 text-center">Description: {product.description}</span>
+                <span className="text-sm text-gray-600 text-center">Price: {product.price}$</span>
+            </div>
+           ))}
         </>
 )
 }
